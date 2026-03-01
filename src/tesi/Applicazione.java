b081a2f1ecd09e8ec155684a1b9971db15ad31f6
riccardopetronio <riccardopetronio2003@gmail.com>
2026-03-01 20:37:32 +0100

    definisco la classe Utente e la classe Applicazione

---
+++ src/tesi/Applicazione.java
@@ -0,0 +1,10 @@
+package tesi;
+
+public class Applicazione {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/tesi/Utente.java
@@ -0,0 +1,5 @@
+package tesi;
+
+public class Utente {
+
+}
