dafcc242b1d0929f89ae53bfd7528b1523c57c41
Riccardo Petronio <riccardopetronio2003@gmail.com>
2026-03-16 19:33:33 +0100

    ho aggiunto il database

--- src/tesi/Applicazione.java
+++ src/tesi/Applicazione.java
@@ -1,5 +1,7 @@
 package tesi;
 
+import java.sql.*;
+
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.profile.AzureProfile;
@@ -20,37 +22,64 @@ public class Applicazione {
         String tenantId = dotenv.get("AZURE_TENANT_ID");
         String clientSecret = dotenv.get("AZURE_CLIENT_SECRET");
         String subscriptionId = dotenv.get("AZURE_SUBSCRIPTION_ID");
-        String resourceGroupName = dotenv.get("AZURE_RESOURCE_GROU");
+        String resourceGroupName = dotenv.get("AZURE_RESOURCE_GROUP");
 
         // Controllo rapido per evitare problemi
         if (clientId == null || tenantId == null || clientSecret == null || subscriptionId == null) {
-            System.err.println("Mancano variabili nel .env. Controlla AZURE_CLIENT_ID, AZURE_TENANT_ID, AZURE_CLIENT_SECRET, AZURE_SUBSCRIPTION_ID");
-            System.exit(1);
+            System.err.println("Mancano variabili nel .env"); // fa stampa rossa per evidenziare che ci sia un'errore
+            System.exit(1); // termina il codice per evitare errori successivi
         }
 
         System.out.println("Tentativo di autenticazione ad Azure usando ClientSecretCredential...");
 
         try {
+        	// 1) creo una entità che rappresenta tutte le credenziali di accesso
             TokenCredential credential = new ClientSecretCredentialBuilder()
                     .clientId(clientId)
                     .tenantId(tenantId)
                     .clientSecret(clientSecret)
                     .build();
 
+            // 2) definisco un'oggetto che rappresenta il mio profilo Azure
             AzureProfile profile = new AzureProfile(tenantId, subscriptionId, AzureEnvironment.AZURE);
 
-            AzureResourceManager azure = AzureResourceManager
+            // 3) con le credenziali creo il manager, ossia l'oggetto che ha potere di azione sulle VM nel profilo
+            AzureResourceManager azureResourceManager = AzureResourceManager
                     .authenticate(credential, profile)
                     .withSubscription(subscriptionId);
+            
+            
+            
+            // ***************** mi connetto al database ********************
+            try {
+            	// sto prendento un database di tipo mysql, salvato in un servere locale (il mio calcolatore), nominato db_tesi e senza password"
+    			String url = "jdbc:mysql://localhost/db_tesi?user=root&password=";		
 
-            System.out.println("Autenticazione riuscita! Elenco VM in: " + resourceGroupName);
-            for (VirtualMachine vm : azure.virtualMachines().listByResourceGroup(resourceGroupName)) {
-                System.out.println("Nome VM: " + vm.name());
-                System.out.println("OS: " + vm.osType());
-                System.out.println("Stato: " + vm.powerState());
-                System.out.println("---------------------------");
-            }
-        } catch (Exception e) {
+    			Connection conn = DriverManager.getConnection(url); // questo oggetto rappresenta la connessione con il database
+    			
+    			PreparedStatement ps = conn.prepareStatement("INSERT INTO virtual_machines(ID, nome, sistema_operativo, stato) VALUES(?, ?, ?, ?)");
+    			
+    			for (VirtualMachine vm : azureResourceManager.virtualMachines().listByResourceGroup(resourceGroupName)) {
+	    	 
+	    	        System.out.println("salvaggio VM: " + vm.name());
+	    			
+	    		    ps.setString(1, vm.vmId());
+	    		    ps.setString(2, vm.name());
+	    		    ps.setString(3, vm.osType().toString());
+	    		    ps.setString(4, vm.powerState().toString());
+	    		    
+	    		    ps.executeUpdate(); // The insert is executed here
+	    		    ps.clearParameters();
+    			}
+    			ps.close();
+    			conn.close();
+    		}
+    		catch (Exception e) {
+                System.err.println("Errore durante la connessione al Database:");
+    			e.printStackTrace();
+    		}     
+        }
+        catch (Exception e) {
             System.err.println("Errore durante la connessione ad Azure:");
             e.printStackTrace();
         }
